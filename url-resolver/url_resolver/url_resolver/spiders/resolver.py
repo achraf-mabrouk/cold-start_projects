0ef7c9107f6219884186e75276e1aed9e697d915
achraf-mabrouk <mabrouk.achraf95@gmail.com>
2022-05-18 20:05:44 +0100

    fixing spider's performance

--- url-resolver/url_resolver/url_resolver/spiders/resolver.py
+++ url-resolver/url_resolver/url_resolver/spiders/resolver.py
@@ -1,12 +1,11 @@
 import scrapy
 import json
-from twisted.internet import reactor, defer
-from scrapy.crawler import CrawlerRunner
-from scrapy.utils.log import configure_logging
+from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 import argparse
+
 parser = argparse.ArgumentParser(description="YouTube videos data")
-parser.add_argument("-i", "--input_path", help="YouTube data input file",required=True)
+parser.add_argument("-i", "--input_path", help="YouTube data input file", required=True)
 parser.add_argument("-o", "--output_path", help="output data directory", required=True)
 args = parser.parse_args()
 
@@ -30,51 +29,52 @@ def load_jsonl(input_path) -> list:
             data.append(json.loads(line.rstrip('\n|\r')))
     return data
 
+
 class ResolverSpider(scrapy.Spider):
     name = 'resolver'
-    record = None
-    resolved_urls = []
-
-    def __init__(self, record):
-        self.record = record  
-        self.urls = iter(self.record['links_in_description'])
-        self.resolved_urls = []
-                
+    data = load_jsonl(args.input_path)
+    
     def start_requests(self):
-        yield scrapy.Request(next(self.urls), 
-                                callback=self.parse,
-                                # meta={'video' : self.record}
-                            )
+        for record in self.data:
+            # add 'resolved_urls' key:value pair to the record                      
+            record['resolved_urls'] = []
+            try:
+                urls = iter(record['links_in_description'])
+                yield scrapy.Request(next(urls), 
+                                        callback=self.parse, 
+                                        meta={'record' : record,
+                                        'urls' : urls                                     
+                                        }
+                                         )
+            # if links_in_description is null                                         
+            except StopIteration:
+                record['resolved_urls'] = None
+                dump_jsonl(record, args.output_path, append=True)
+
         
     def parse(self, response):
+        record = response.meta['record']
+        urls = response.meta['urls']
         # resolving 
         canonical_url = response.xpath('//link[@rel="canonical"]/@href').get()
         if canonical_url is None or "{{SEO.url}}":
             canonical_url = response.url
         
-        self.resolved_urls.append(canonical_url)
-        
+        record['resolved_urls'].append(canonical_url)
         try:
-            url = next(self.urls)
-            yield response.follow(url, callback=self.parse) 
-        except StopIteration:
-            self.record['resolved_urls'] = self.resolved_urls
-            dump_jsonl(self.record, args.output_path, append=True)
+            url = next(urls)
+            yield response.follow(url, 
+                                callback=self.parse,  
+                                meta={'record' : record,
+                                'urls' : urls }) 
+        except StopIteration:            
+            dump_jsonl(record, args.output_path, append=True)
+            
 
 
-input_path = args.input_path
-output_path = args.output_path
-configure_logging()
 settings = get_project_settings()
-runner = CrawlerRunner(settings)
+process = CrawlerProcess(settings)
 
-@defer.inlineCallbacks
-def crawl():
-    with open(input_path, 'r', encoding='utf-8') as f:
-        for line in f:
-            record = json.loads(line.rstrip('\n|\r'))
-            yield runner.crawl(ResolverSpider, record)
-        reactor.stop()
+process.crawl(ResolverSpider)
 
-crawl()
-reactor.run() # the script will block here until the last crawl call is finished
+process.start() # the script will block here until the crawling is finished
