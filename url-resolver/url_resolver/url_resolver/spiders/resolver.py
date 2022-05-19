9db33854416f17cb43296e31daf8af39cf463ba8
achraf-mabrouk <mabrouk.achraf95@gmail.com>
2022-05-19 13:14:24 +0100

    chagin the spider

--- url-resolver/url_resolver/url_resolver/spiders/resolver.py
+++ url-resolver/url_resolver/url_resolver/spiders/resolver.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser(description="YouTube videos data")
 parser.add_argument("-i", "--input_path", help="YouTube data input file", required=True)
 parser.add_argument("-o", "--output_path", help="output data directory", required=True)
 args = parser.parse_args()
+# input_path = "/home/frantic95/cold-start/url-resolver/url_resolver/input/chunk1.jsonl"
 
 def dump_jsonl(json_record, output_path, append=False):
     """
@@ -33,7 +34,7 @@ def load_jsonl(input_path) -> list:
 class ResolverSpider(scrapy.Spider):
     name = 'resolver'
     data = load_jsonl(args.input_path)
-    
+
     def start_requests(self):
         for record in self.data:
             # add 'resolved_urls' key:value pair to the record                      
@@ -41,17 +42,18 @@ class ResolverSpider(scrapy.Spider):
             try:
                 urls = iter(record['links_in_description'])
                 yield scrapy.Request(next(urls), 
-                                        callback=self.parse, 
-                                        meta={'record' : record,
+                                    callback=self.parse, 
+                                    meta={'record' : record,
                                         'urls' : urls                                     
-                                        }
-                                         )
+                                        },
+                                    errback=self.errback_handler
+                                    )
             # if links_in_description is null                                         
             except StopIteration:
                 record['resolved_urls'] = None
                 dump_jsonl(record, args.output_path, append=True)
-
-        
+                # yield record
+       
     def parse(self, response):
         record = response.meta['record']
         urls = response.meta['urls']
@@ -66,13 +68,37 @@ class ResolverSpider(scrapy.Spider):
             yield response.follow(url, 
                                 callback=self.parse,  
                                 meta={'record' : record,
-                                'urls' : urls }) 
+                                'urls' : urls },
+                                errback=self.errback_handler
+                                ) 
         except StopIteration:            
             dump_jsonl(record, args.output_path, append=True)
-            
+            # yield record
+        
+    def errback_handler(self, response, failure):
+        record = response.meta['record']
+        urls = response.meta['urls']
+        record['resolved_urls'].append(None)
+        try:
+            yield response.follow(next(urls), 
+                                callback=self.parse,  
+                                meta={'record' : record,
+                                'urls' : urls },
+                                errback=self.errback_handler
+                                )
+        except StopIteration:            
+            dump_jsonl(record, args.output_path, append=True) 
+            # yield record
+    
 
 
 settings = get_project_settings()
+# # settings={
+# #     "FEEDS": {
+# #         "items.json": {"format": "json"},
+
+# #     },
+# # }
 process = CrawlerProcess(settings)
 
 process.crawl(ResolverSpider)
